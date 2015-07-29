d68cb29ec29f55315e0641fb7066ddb729e04223
weiderson <weiderson.mendes@gmail.com>
2015-07-28 23:54:06 -0300

    Commit in 2807 - V0.02

--- MvvmLifeStyle/ViewModel/MainViewModel.cs
+++ MvvmLifeStyle/ViewModel/MainViewModel.cs
@@ -3,49 +3,47 @@ using MvvmLifeStyle.Model;
 
 namespace MvvmLifeStyle.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
+    
     public class MainViewModel : ViewModelBase
     {
-        private readonly IDataService _dataService;
+        private readonly IDataService _dataService;       
+        public const string WelcomeTitlePropertyName = "Seja bem Vindo";
+        public const string WelcomeTitlePropertyMensagem1 = "Mensagem";
 
-        /// <summary>
-        /// The <see cref="WelcomeTitle" /> property's name.
-        /// </summary>
-        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+        private string _mensagem0 = string.Empty;
+        private string _mensagem1 = string.Empty;
 
-        private string _welcomeTitle = string.Empty;
 
-        /// <summary>
-        /// Gets the WelcomeTitle property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string WelcomeTitle
+        public string Mensagem0
         {
-            get
-            {
-                return _welcomeTitle;
-            }
+            get { return _mensagem0; }
+
+            set {
+                if (_mensagem0 == value)
+                    {
+                        return;
+                    }
+                    _mensagem0 = value;
+                    RaisePropertyChanged(WelcomeTitlePropertyName);
+                }
+        }
+
+        public string Mensagem1
+        {
+            get { return _mensagem1; }
 
             set
             {
-                if (_welcomeTitle == value)
+                if (_mensagem1 == value)
                 {
                     return;
                 }
-
-                _welcomeTitle = value;
-                RaisePropertyChanged(WelcomeTitlePropertyName);
+                _mensagem1 = value;
+                RaisePropertyChanged(WelcomeTitlePropertyMensagem1);
             }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        
         public MainViewModel(IDataService dataService)
         {
             _dataService = dataService;
@@ -57,16 +55,11 @@ namespace MvvmLifeStyle.ViewModel
                         // Report error here
                         return;
                     }
+                    _mensagem0 = item.Title;
+                    _mensagem1 = item.Title;
 
-                    WelcomeTitle = item.Title;
                 });
         }
-
-        ////public override void Cleanup()
-        ////{
-        ////    // Clean up if needed
-
-        ////    base.Cleanup();
-        ////}
+       
     }
 }
--- MvvmLifeStyle/ViewModel/ViewModelLocator.cs
+++ MvvmLifeStyle/ViewModel/ViewModelLocator.cs
@@ -1,28 +1,10 @@
-﻿/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:MvvmLifeStyle.ViewModel"
-                                   x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-*/
-
-using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 using MvvmLifeStyle.Model;
 
 namespace MvvmLifeStyle.ViewModel
-{
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
+{  
     public class ViewModelLocator
     {
         static ViewModelLocator()
@@ -41,9 +23,7 @@ namespace MvvmLifeStyle.ViewModel
             SimpleIoc.Default.Register<MainViewModel>();
         }
 
-        /// <summary>
-        /// Gets the Main property.
-        /// </summary>
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
             "CA1822:MarkMembersAsStatic",
             Justification = "This non-static member is needed for data binding purposes.")]
@@ -55,9 +35,7 @@ namespace MvvmLifeStyle.ViewModel
             }
         }
 
-        /// <summary>
-        /// Cleans up all the resources.
-        /// </summary>
+       
         public static void Cleanup()
         {
         }
